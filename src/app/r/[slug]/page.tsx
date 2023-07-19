48996274b56216406f8bedcb02b6b1b5d9ee5585
fernandobcc <fernandoxico@gmail.com>
2023-07-19 19:09:35 -0300

    feat: add create community page

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import { INFINITE_SCROLLING_PAG_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
+
+interface PageProps {
+  params: {
+    slug: string
+  }
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { slug } = params
+
+  const session = await getAuthSession()
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true
+        },
+        orderBy: {
+          createdAt: 'desc'
+        },
+        take: INFINITE_SCROLLING_PAG_RESULTS
+      }
+    }
+  })
+
+  if (!subreddit) return notFound()
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{subreddit.name}
+      </h1>
+    </>
+  )
+}
+
+export default Page
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const INFINITE_SCROLLING_PAG_RESULTS = 2
